664b8fbf824f94bc4337eace0a0073ea9ea65da1
alekseytsvetkov <code.aleksey@icloud.com>
2024-03-05 22:20:02 +0300

    chore(no-ref): added auth package

--- web/packages/auth/src/server.ts
+++ web/packages/auth/src/server.ts
@@ -1,130 +1,124 @@
-/**
- * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1).
- * 2. You want to create a new middleware or type of procedure (see Part 3).
- *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
- */
-
-import { initTRPC, TRPCError } from '@trpc/server';
-import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
-import { type Session } from 'next-auth';
-import superjson from 'superjson';
-import { ZodError } from 'zod';
-
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import type { Role, RoleTypes, User } from '@store/db/types';
+import DiscordProvider from 'next-auth/providers/discord';
 import { prisma } from '@store/db';
-import { getServerAuthSession } from './auth';
+import NextAuth from './next-auth';
+
+export type { Session, DefaultSession as DefaultAuthSession } from 'next-auth';
 
 /**
- * 1. CONTEXT
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
  *
- * This section defines the "contexts" that are available in the backend API.
- *
- * These allow you to access things when processing a request, like the database, the session, etc.
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-
-interface CreateContextOptions {
-  session: Session | null;
+declare module 'next-auth' {
+  interface Session {
+    user: User & { role: RoleTypes[] };
+  }
 }
 
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
- */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
-  return {
-    session: opts.session,
-    prisma,
-  };
-};
-
-/**
- * This is the actual context you will use in your router. It will be used to process every request
- * that goes through your tRPC endpoint.
- *
- * @see https://trpc.io/docs/context
- */
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
-
-  // Get the session from the server using the getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
-
-  return createInnerTRPCContext({
-    session,
-  });
-};
-
-/**
- * 2. INITIALIZATION
- *
- * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
- * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
- * errors on the backend.
- */
-
-const t = initTRPC.context<typeof createTRPCContext>().create({
-  transformer: superjson,
-  errorFormatter({ shape, error }) {
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
-      },
-    };
-  },
-});
-
-/**
- * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
- *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
- * "/src/server/api/routers" directory.
- */
-
-/**
- * This is how you create new routers and sub-routers in your tRPC API.
- *
- * @see https://trpc.io/docs/router
- */
-export const createTRPCRouter = t.router;
-
-/**
- * Public (unauthenticated) procedure
- *
- * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
- * guarantee that a user querying is authorized, but you can still access user session data if they
- * are logged in.
- */
-export const publicProcedure = t.procedure;
-
-/** Reusable middleware that enforces users are logged in before running the procedure. */
-const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session?.user) {
-    throw new TRPCError({ code: 'UNAUTHORIZED' });
+declare module '@auth/core/adapters' {
+  interface AdapterUser extends User {
+    roles: Role[];
   }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
-});
+}
 
-/**
- * Protected (authenticated) procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
- *
- * @see https://trpc.io/docs/procedures
- */
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
+if (!process.env.DISCORD_CLIENT_ID) {
+  throw new Error('No DISCORD_CLIENT_ID has been provided.');
+}
+
+if (!process.env.DISCORD_CLIENT_SECRET) {
+  throw new Error('No DISCORD_CLIENT_SECRET has been provided.');
+}
+
+const useSecureCookies = process.env.VERCEL_ENV === 'production';
+const cookiePrefix = useSecureCookies ? '__Secure-' : '';
+const cookieDomain = useSecureCookies ? 'store.dev' : undefined;
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  pages: {
+    signIn: '/login',
+  },
+  cookies: {
+    sessionToken: {
+      name: `${cookiePrefix}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        domain: cookieDomain,
+        secure: useSecureCookies,
+      },
+    },
+  },
+  adapter: {
+    ...PrismaAdapter(prisma),
+    // Override createUser method to add default USER role
+    createUser: async (data) => {
+      const user = await prisma.user.create({
+        data: {
+          ...data,
+          name: data.name ?? '',
+          roles: {
+            connectOrCreate: {
+              where: { role: 'USER' },
+              create: { role: 'USER' },
+            },
+          },
+        },
+        include: { roles: true },
+      });
+      return user;
+    },
+    // Override getSessionAndUser method to include roles. Avoids a second db query in session callback
+    getSessionAndUser: async (sessionToken) => {
+      const userAndSession = await prisma.session.findUnique({
+        where: { sessionToken },
+        include: { user: { include: { roles: true } } },
+      });
+      if (!userAndSession) return null;
+      const { user, ...session } = userAndSession;
+      return { user, session };
+    },
+  },
+  callbacks: {
+    session: async (opts) => {
+      if (!('user' in opts)) throw new Error("unreachable, we're not using JWT");
+
+      const { session, user } = opts;
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          role: user.roles.map((r) => r.role),
+        },
+      };
+    },
+  },
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      profile(profile) {
+        if (profile.avatar === null) {
+          const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+        } else {
+          const format = profile.avatar.startsWith("a_") ? "gif" : "png"
+          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
+        }
+        return {
+          id: profile.id,
+          name: profile.username,
+          email: profile.email,
+          image: profile.image_url,
+        }
+      },
+    }),
+  ],
+});
